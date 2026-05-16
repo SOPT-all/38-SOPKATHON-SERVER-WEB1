1ba75667d8484eec0a772c4da7644a3722cf94fb
5eoyng <szoyoung@naver.com>
2026-05-17 01:37:31 +0900

    feat(#9): Message, SaveMessage 엔티티 작성

---
+++ src/main/java/org/sopt/sopkathon_server/domain/message/entity/SavedMessage.java
@@ -0,0 +1,44 @@
+package org.sopt.sopkathon_server.domain.message.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.sopt.sopkathon_server.global.entity.BaseTimeEntity;
+
+@Entity
+@Table(name = "saved_message")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SavedMessage extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String senderInitial;
+
+    @Column(nullable = false)
+    private String receiverInitial;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "message_id", unique = true)
+    private Message message;
+
+    @Builder
+    private SavedMessage(String senderInitial, String receiverInitial, String content, String passwordHash, Message message) {
+        this.senderInitial = senderInitial;
+        this.receiverInitial = receiverInitial;
+        this.content = content;
+        this.passwordHash = passwordHash;
+        this.message = message;
+    }
+}
