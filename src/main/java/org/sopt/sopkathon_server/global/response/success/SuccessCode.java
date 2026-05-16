766743aff8374ca56d1649e3efb233f2f06b0a80
5eoyng <szoyoung@naver.com>
2026-05-17 02:29:54 +0900

    feat(#9): 성공 코드 추가

--- src/main/java/org/sopt/sopkathon_server/global/response/success/SuccessCode.java
+++ src/main/java/org/sopt/sopkathon_server/global/response/success/SuccessCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum SuccessCode implements SuccessType {
     // 공통 응답 코드
-    SUCCESS(200, "CMN_200", "요청이 성공했습니다.");
+    SUCCESS(200, "CMN_200", "요청이 성공했습니다."),
+    CREATED(201, "CMN_201", "생성되었습니다.");
 
     private final int status;
     private final String code;
