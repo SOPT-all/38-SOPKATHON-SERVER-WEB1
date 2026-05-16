7dde180ae9bdf36b6cd3a199109c0b9e6fc1c851
1siis1 <sisees01@gmail.com>
2026-05-17 03:03:54 +0900

    feat: 도메인 에러 코드 추가

--- src/main/java/org/sopt/sopkathon_server/global/response/error/ErrorCode.java
+++ src/main/java/org/sopt/sopkathon_server/global/response/error/ErrorCode.java
@@ -13,8 +13,13 @@ public enum ErrorCode implements ErrorType {
     MISSING_PARAMETER(400, "CMN_400_2", "필수 파라미터가 누락되었습니다."),
     INVALID_FORMAT(400, "CMN_400_3", "요청 데이터 형식이 올바르지 않습니다."),
 
+    // 401 Unauthorized
+    INVALID_PASSWORD(401, "PWD_401", "비밀번호가 틀렸습니다."),
+
     // 404 Not Found
     NOT_FOUND(404, "CMN_404", "요청한 리소스를 찾을 수 없습니다."),
+    MESSAGE_NOT_FOUND(404, "MSG_404", "messageId에 해당하는 메시지가 없습니다."),
+    SAVED_MESSAGE_NOT_FOUND(404, "SMG_404", "savedMessageId에 해당하는 메시지가 없습니다."),
 
     // 405 Method Not Allowed
     METHOD_NOT_ALLOWED(405, "CMN_405", "허용되지 않은 HTTP 메서드입니다."),
