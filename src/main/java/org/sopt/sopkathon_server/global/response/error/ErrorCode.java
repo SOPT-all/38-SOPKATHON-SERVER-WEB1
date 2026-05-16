35582e7ba9893047edc75e451185051862ddd296
1siis1 <sisees01@gmail.com>
2026-05-17 05:23:47 +0900

    chore: 에러코드 중복 제거

--- src/main/java/org/sopt/sopkathon_server/global/response/error/ErrorCode.java
+++ src/main/java/org/sopt/sopkathon_server/global/response/error/ErrorCode.java
@@ -29,7 +29,6 @@ public enum ErrorCode implements ErrorType {
 
 
     //Message
-    MESSAGE_NOT_FOUND(404, "MSG_404", "존재하지 않는 메시지입니다."),
     REPLY_NOT_ALLOWED(400, "MSG_400", "답장에는 답장할 수 없습니다.");
 
     private final int status;
