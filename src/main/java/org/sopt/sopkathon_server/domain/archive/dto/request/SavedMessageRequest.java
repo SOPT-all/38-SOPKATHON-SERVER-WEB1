5b90377df6ad1a3b5f293c0a914853110386b6a5
1siis1 <sisees01@gmail.com>
2026-05-17 04:36:37 +0900

    feat: swagger schema 정리

--- src/main/java/org/sopt/sopkathon_server/domain/archive/dto/request/SavedMessageRequest.java
+++ src/main/java/org/sopt/sopkathon_server/domain/archive/dto/request/SavedMessageRequest.java
@@ -1,11 +1,15 @@
 package org.sopt.sopkathon_server.domain.archive.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record SavedMessageRequest (
+        @Schema(description = "저장하고 싶은 message의 id입니다.", example = "1")
         @NotNull
         Long messageId,
+
+        @Schema(description = "저장하기 위한 비밀번호입니다.", example = "1234")
         @NotBlank
         String password
 ) {
